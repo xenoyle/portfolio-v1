56a6f159d46324fbbf705cc7da116a43b93fcc1c
Connor Floyd <cwfloyd@email.uscb.edu>
2024-10-17 17:01:34 -0400

    button animations implemented

--- script.js
+++ script.js
@@ -4,3 +4,61 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+function adjustScroll() {
+  if (window.location.hash) {
+    const headerHeight = document.querySelector('#desktop-nav').offsetHeight;
+    window.scrollTo(0, window.pageYOffset - headerHeight);
+  }
+}
+  
+// Call this function when the page loads and when the hash changes
+window.addEventListener('load', adjustScroll);
+window.addEventListener('hashchange', adjustScroll);
+
+// Smooth scroll function
+function smoothScroll(target, duration) {
+  var target = document.querySelector(target);
+  var targetPosition = target.getBoundingClientRect().top;
+  var startPosition = window.pageYOffset;
+  var headerOffset = document.querySelector('#desktop-nav').offsetHeight;
+  var additionalOffset = -40; // Adjust this value as needed
+  var distance = targetPosition - headerOffset - additionalOffset;
+  var startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  // Easing function
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
+// Add click event listeners to all anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    var target = this.getAttribute('href');
+    smoothScroll(target, 1000); // 1000ms duration of scroll animation
+  });
+});
+
+// Adjust scroll position on page load if there's a hash in the URL
+window.addEventListener('load', function() {
+  if (window.location.hash) {
+    setTimeout(function() {
+      smoothScroll(window.location.hash, 1);
+    }, 0);
+  }
+});
